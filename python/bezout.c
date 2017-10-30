f457e23967fa68a075b58268a8240fdcee8db170
jpcp13 <jpcp13@gmail.com>
2017-10-30 23:06:03 +0100

    updated bezout.c

--- python/bezout.c
+++ python/bezout.c
@@ -3,20 +3,15 @@
 #include <math.h>
 #include <time.h>
 
-#define foreach(a, b, c) for (int a = b; a < c; a++)
-#define for_i foreach(i, 0, n)
-#define for_j foreach(j, 0, n)
-#define for_ij for_i for_j
-
 struct gcd {
-    int x, y, d;
+    long long x, y, d;
 };
  
 typedef struct gcd myGcd;
  
-myGcd bezout_gcd(int a, int b){
+myGcd bezout_gcd(long long a, long long b){
 	myGcd g;
-	int s, t, r, old_s, old_t, old_r, quotient, temp;
+	long long s, t, r, old_s, old_t, old_r, quotient, temp;
 	s = 0; old_s = 1;
 	t = 1; old_t = 0;
 	r = b; old_r = a;
@@ -36,72 +31,91 @@ myGcd bezout_gcd(int a, int b){
     return g;
 }
 
-int* readB(int n) {
+long long* readB(int n, int D) {
 	FILE *f;
-	int *B;
-    B = (int*) malloc(sizeof(int)*n*n);
-	f = fopen("b0.txt", "r");
-	for_ij{
-    	fscanf(f, "%d", B + n*i + j);
+	long long *B;
+    B = (long long*) malloc(sizeof(long long)*(n+1)*D*D);
+	f = fopen("bb.txt", "r");
+	for (int k = 0; k < n+1; k++) {
+		for (int i = 0; i < D; i++) {
+			for (int j = 0; j < D; j++) {
+    			fscanf(f, "%lli", B + D*D*k + D*i + j);
+			}
+		}
     }
 	fclose(f);
 	return B;
 }
 
-void writeB(int n, int i_max, int j_max, int *B) {
+void writeB(int n, int D, long long *B) {
 	FILE *f;
-	f = fopen("b1.txt", "w");
-	for (int i = 0; i < i_max; i++) {
-		for (int j = 0; j < j_max; j++) {
-    		fprintf(f, "%d ", B[n*i + j]);
+	f = fopen("bb1.txt", "w");
+	for (int k = 0; k < n+1; k++) {
+		for (int i = 0; i < D; i++) {
+			for (int j = 0; j < D; j++) {
+				fprintf(f, "%lli ", B[D*D*k + D*i + j]);
+			}
+			fprintf(f, "\n");
 		}
-		fprintf(f, "\n");
-    }
+	}
 	fclose(f);
 }
-void process_lines(int i0, int i1, int j0, int n, int *B) {
-	int a, b, temp;
+
+void process_lines(int i0, int i1, int j0, int n, int D, long long *B) {
+	long long a, b, temp;
 	myGcd g;
-	a = B[n*i0 + j0];
-	b = B[n*i1 + j0];
+	a = B[D*i0 + j0];
+	b = B[D*i1 + j0];
 	g = bezout_gcd(a, b);
 	if (g.d != 0) {
-		for (int j = 0; j < n; j++) {
-			int aj = B[n*i0 + j];
-			int bj = B[n*i1 + j];
-			temp = g.x*aj + g.y*bj;
-			B[n*i1 + j] = -b/g.d*aj + a/g.d*bj;
-			B[n*i0 + j] = temp;
+		for (int k = 0; k < n+1; k++) {
+			for (int j = 0; j < D; j++) {
+				long long aj = B[D*D*k + D*i0 + j];
+				long long bj = B[D*D*k + D*i1 + j];
+				temp = g.x*aj + g.y*bj;
+				B[D*D*k + D*i1 + j] = -b/g.d*aj + a/g.d*bj;
+				B[D*D*k + D*i0 + j] = temp;
+			}
 		}
 	}
 }
 
-void process_block(int block_start, int block_end, int n, int *B) {
+void process_block(int block_start, int block_end, int n, int D, long long *B) {
 	for (int j0 = block_start; j0 < block_end; j0++) {
-		for (int i1 = j0 + 1; i1 < block_end; i1++) {
-			process_lines(j0, i1, j0, n, B);
+		for (int i1 = block_end-1; i1 > j0; i1--) {
+			process_lines(i1-1, i1, j0, n, D, B);
 		}
-		printf("%d ", B[n*j0 + j0]);
+		/*for (int i1 = j0 + 1; i1 < block_end; i1++) {
+			process_lines(j0, i1, j0, n, B);
+		}*/
 	}
 }
 
 int main(){
 	double cpu_time_used, start, end;
-	int n = 48, block_size = 8;
-	int *B;
-	B = readB(n);
+	int n = 2;
+	int D = 32;
+	int block_size = 4;
+	long long *B;
+	B = readB(n, D);
+
+
 	start = clock();
-	for (int block_start = 0; block_start < n; block_start += block_size) {
-		process_block(block_start, block_start + block_size, n, B);
+	for (int block_start = 0; block_start < D; block_start += block_size) {
+		//printf("block_start = %d, block_end = %d\n", block_start, block_start + block_size);
+		int block_end = block_start + block_size;
+		process_block(block_start, block_end, n, D, B);
 	}
 	end = clock();
 	cpu_time_used = (end - start) / CLOCKS_PER_SEC;
 	printf("cpu_time_used = %f\n", cpu_time_used);
 
-	writeB(n, n, n, B);
+
+
+	writeB(n, D, B);
 /*
 	for_j {
-		printf("%d ", B[n*0 + j]);
+		printf("%lli ", B[n*0 + j]);
 	}
 */
 	
