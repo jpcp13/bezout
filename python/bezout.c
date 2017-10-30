8c4a6b8d8af3dbb28147bda09bf6b29a59cb4de8
jpcp13 <jpcp13@gmail.com>
2017-10-30 12:54:28 +0100

    updated bezout.c

--- python/bezout.c
+++ python/bezout.c
@@ -22,13 +22,11 @@ myGcd bezout_gcd(int a, int b){
 	t = 1; old_t = 0;
 	r = b; old_r = a;
 	while(r != 0) {
+
 	    quotient = old_r/r;
-	    // We are overriding the value of r, before that we store it"s current
-	    // value in temp variable, later we assign it to old_r
 	    temp = r;
 	    r = old_r - quotient*r;
 	    old_r = temp;
-	    // We treat s and t in the same manner we treated r
 	    temp = s;
 	    s = old_s - quotient*s;
 	    old_s = temp;
@@ -39,43 +37,76 @@ myGcd bezout_gcd(int a, int b){
 	g.x = old_s; g.y = old_t; g.d = old_r;
     return g;
 }
- 
 
+int* readB(int n) {
+	FILE *f;
+	int *B;
+    B = (int*) malloc(sizeof(int)*n*n);
+	f = fopen("b0.txt", "r");
+	for_ij{
+    	fscanf(f, "%d", B + n*i + j);
+    }
+	fclose(f);
+	return B;
 
+}
+
+void writeB(int n, int *B) {
+	FILE *f;
+	f = fopen("b1.txt", "w");
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+    		fprintf(f, "%d ", B[n*i + j]);
+		}
+		fprintf(f, "\n");
+    }
+	fclose(f);
+
+}
+void  process_lines(int i0, int i1, int n, int *B) {
+
+	int a, b, temp;
+	myGcd g;
+	a = B[n*i0];
+	b = B[n*i1];
+
+	g = bezout_gcd(a, b);
+
+	for_j {
+		int aj = B[n*i0 + j];
+		int bj = B[n*i1 + j];
+		temp = g.x*aj + g.y*bj;
+		B[n*i1 + j] = -b/g.d*aj + a/g.d*bj;
+		B[n*i0 + j] = temp;
+	}
+
+}
 
 
 int main(){
 	double cpu_time_used, start, end;
+
 	int n = 720;
-	FILE *f;
-
-
 	int *B;
-    B = (int*) malloc(sizeof(int)*n*n);
-	f = fopen("b0.txt", "r");
+	B = readB(n);
+
 	start = clock();
-	for_ij{
-    	fscanf(f, "%d", B + i*n + j);
-    }
+	int i0 = 0, i1_max = 10;
+	for (int i1 = 1; i1 < i1_max; i1++) {
+		process_lines(i0, i1, n, B);
+	}
 	end = clock();
 	cpu_time_used = (end - start) / CLOCKS_PER_SEC;
-	printf("cpu_time_matrix = %f\n", cpu_time_used);
-/*
-	for_i{
+	printf("cpu_time_used = %f\n", cpu_time_used);
 
-			printf("%d ", *(B + i*n + i));
-		printf(" ");
+	writeB(n, B);
+/*
+	for_j {
+		printf("%d ", B[n*0 + j]);
 	}
 */
-	int a, b;
-	myGcd g;
-	a = 95642;
-	b = 1681;
-	start = clock();
-	g = bezout_gcd(a, b);
-	end = clock();
-	cpu_time_used = (end - start) / CLOCKS_PER_SEC;
-	printf("cpu_time_gcd = %f\n", cpu_time_used);
-	printf("x = %d, y = %d, gcd = %d", g.x, g.y, g.d);
+	
+
+
 	return 0;
 }
